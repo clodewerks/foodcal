b07094f44000514d8952b32956bade65a0e6d0f4
Kyle Linneman <klinneman@navexglobal.com>
2019-09-03 20:59:28 -0700

    Fixing stray old mysql function

--- jsonservices/addMeal.php
+++ jsonservices/addMeal.php
@@ -4,7 +4,7 @@ include '../php/sql.php';
 if (!isset($_REQUEST["mealName"])) echo 'No name set!';
 else {
 
-	$mealDates = mysqli_query($con,"insert meals (Meal_name,Meal_Active) values ('".mysql_real_escape_string($con,$_REQUEST["mealName"])."',1);") or die(mysqli_error());
+	$mealDates = mysqli_query($con,"insert meals (Meal_name,Meal_Active) values ('".mysqli_real_escape_string($con,$_REQUEST["mealName"])."',1);") or die(mysqli_error());
 
 	echo 'Meal Added!';
 
