1c290d2ef023b32465adf386b3e48c17f9ffc951
Kyle Linneman <klinneman@navexglobal.com>
2021-02-08 11:28:33 -0800

    Defaulting Weekends (Friday and Saturday) to eating out.

--- jsonservices/addDates.php
+++ jsonservices/addDates.php
@@ -4,13 +4,14 @@ include '../php/sql.php';
 
 $query =  'insert ignore into calendar (`meal_date`, `Meal`, `Meal_id`) VALUES ';
 while ($monthViewStart <= $monthViewEND) {
-    $query = $query.' (\''.date('Y-m-d',$monthViewStart).'\',\'\',\'12\')';
+    if(getWeekday($monthViewStart) >= 5 ) $query = $query.' (\''.date('Y-m-d',$monthViewStart).'\',\'\',\'6\')';
+    else $query = $query.' (\''.date('Y-m-d',$monthViewStart).'\',\'\',\'12\')';
     if($monthViewStart < $monthViewEND) $query = $query.',';
     else $query = $query.'';
     $monthViewStart = strtotime(date('Y-m-d',$monthViewStart).'+ 1 day');
 }
 $mealDates = mysqli_query($con,$query) or die(mysqli_error());
 
-echo 'Dates Added!';
+echo 'Dates Added!!';
 
 ?>
